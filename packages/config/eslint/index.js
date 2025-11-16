5bc2746adbfbb80bf32b671378c325167664f805
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2025-11-16 14:02:07 +0100

    SPM-34 style: standardize ESLint configuration by unifying quote styles and removing unnecessary rules

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -2,11 +2,6 @@ module.exports = {
   extends: ["next/core-web-vitals", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
-    quotes: [
-      "error",
-      "single",
-      { avoidEscape: true, allowTemplateLiterals: false },
-    ],
   },
   parserOptions: {
     babelOptions: {
